792e0f16cb3bde84f0337dd26dd481e966115f16
michaelvelardeb <michaelvelarde@yahoo.com>
2021-02-07 16:40:14 -0800

    add actionListener for password page add button

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         MainForm mainForm = new MainForm();
-        addPasswordForm passwordForm = new addPasswordForm();
-
         mainForm.setVisible(true);
 
     }
--- src/main/java/textForm.java
+++ src/main/java/textForm.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class textForm extends JFrame {
 
@@ -20,8 +22,32 @@ public class textForm extends JFrame {
     public textForm() {
 
         this.add(formPanel);
-
         this.setSize(450, 400);
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Password passwordElement = new Password();
+                passwordElement.setTitle(nameTextField.getText());
+                passwordElement.setPassword(passwordField.getText());
+                passwordElement.setCategory(categoryField.getText());
+                passwordElement.setSecurityAnswer(securityAnswerField.getText());
+                passwordElement.setSecurityQuestion(securityField.getText());
+                passwordElement.setHyperLink(hyperLinkField.getText());
+
+                notesArea.append(passwordElement.title+  "\n");
+                notesArea.append(passwordElement.password + "\n");
+                notesArea.append(passwordElement.category + "\n");
+                notesArea.append(passwordElement.securityQuestion + "\n");
+                notesArea.append(passwordElement.securityAnswer + "\n");
+                notesArea.append(passwordElement.hyperLink + "\n");
+
+                textForm.super.dispose();
+
+
+            }
+        });
 
     }
 }
